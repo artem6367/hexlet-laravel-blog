f09cc82e8b18fcb31c5b41fe99a1d70f5e23a8b9
artem6367 <artem_ivan1998@mail.ru>
2024-06-18 13:38:36 +0700

    Добавлена страница about

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/qwe', function () {
+    return 'qwe';
+});
+
+Route::get('about', function () {
+    return view('about');
+});
