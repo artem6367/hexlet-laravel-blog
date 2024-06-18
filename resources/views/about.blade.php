4ad7a43e4e7f30657b6d41408d7a85777e2d3289
artem6367 <artem_ivan1998@mail.ru>
2024-06-18 14:37:47 +0700

    Добавлен макет app

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,9 @@
-<h1>О блоге</h1>
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('header', 'О блоге')
+
+@section('content')
 <p>Эксперименты с Laravel на Хекслете</p>
+@endsection
